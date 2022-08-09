e630f251d31f0ffaf6116b3358ce4d3e9502d97d
jenna-graham <jclayto2@gmail.com>
2022-08-09 15:26:10 -0700

    POST route signs in an existing user TEST passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,8 +6,8 @@ const app = require('../lib/app');
 const newUser = {
   firstName: 'Test',
   lastName: 'User',
-  email: 'test@example.com',
-  password: '123456',
+  email: 'test@jenna.com',
+  password: '12345',
 };
 describe('backend-express-template routes', () => {
   beforeEach(() => {
@@ -24,6 +24,14 @@ describe('backend-express-template routes', () => {
       lastName,
       email,
     });
+  });
+
+  it('signs in an existing user', async () => {
+    await request(app).post('/api/v1/users').send(newUser);
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'test@jenna.com', password: '12345' });
+    expect(res.status).toEqual(200);
   });
 
   afterAll(() => {
